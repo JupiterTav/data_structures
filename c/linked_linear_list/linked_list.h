34db9c605ed45f357ce305da64f87d592db18cbb
JupiterTav <wesleyTavaresDev@outlook.com>
2025-07-28 17:11:48 -0300

    linked_list.h

---
+++ c/linked_linear_list/linked_list.h
@@ -0,0 +1,14 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+typedef struct list{
+  int node;
+  struct list *next;
+}list;
+
+list *search_list(list *l, int item);
+
+void insert_list(list **l, int item);
+
+void delete_from_list(list **l, int item);
+#endif
